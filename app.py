ae651730de2f09d6200a8fcd8d7ff1512bc7ee92
Abraham <abraham.95io31@gmail.com>
2025-07-30 13:10:41 +0630

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def generate_cards_from_df(df, output_folder):
 
             if email:
                 subject = f"Your A-Member Card Awaits You"
-                send_email_with_attachment(email, subject, "", filename)
+                send_email_clean(email, subject, "", filename)
 
 def zip_folder(folder_path, zip_path):
     with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
